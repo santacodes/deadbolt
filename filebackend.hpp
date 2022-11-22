162f3c9cb4424cc286e8037952217d6c3adbb742
Santhosh <52504160+santacodes@users.noreply.github.com>
2022-11-22 23:01:12 +0530

    adding sqlite

--- filebackend.hpp
+++ filebackend.hpp
@@ -1,11 +1,27 @@
 #pragma once
-
+//To create and edit in the sqlite db
 #include <string>
+#include <sqlite3>
+
 
 class filebackend{
 public: 
+	sqlite3* DB;
+    std::string sql = "CREATE TABLE PASSWORDS("
+                      "ID INT PRIMARY KEY     NOT NULL, "
+                      "USERNAME           TEXT    NOT NULL, "
+                      "PASSWORD          TEXT     NOT NULL;";
 void create(){
 	printf("created the file");
 
 	}
+void append(){
+	printf("Appending to the file..");
+
+}	
+void del(){
+	printf("Deleting the file ")
+
+}
+
 };
