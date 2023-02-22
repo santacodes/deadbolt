f44623f41e4af328c3142fd5680044edb9244d08
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-02-22 21:20:21 +0530

    debugged

--- cli.cpp
+++ cli.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 #include <map>
-#include <unistd.h>
+#include <unistd.h> //for (unix/linux)
 #include "filebackend.hpp"
 
 /*
@@ -11,63 +11,68 @@
 --- DOCUMENTATION ---
 
 class options{
-	bool --help;
-  	std::string --greeting = "Hello";
-  	unsigned int --exclamations = 1;
+    bool --help;
+    std::string --greeting = "Hello";
+    unsigned int --exclamations = 1;
 std::cout << "List of arguments - " << std::endl;
-				std::cout << "-c, -C, -create => To Create a new Encrypted note" << std::endl;
-				std::cout << "-e, -E, -edit => To edit an existing note" << std::endl;
-				std::cout << "-d, -D, -delete => To delete an exsiting note" << std::endl;
+                std::cout << "-c, -C, -create => To Create a new Encrypted note" << std::endl;
+                std::cout << "-e, -E, -edit => To edit an existing note" << std::endl;
+                std::cout << "-d, -D, -delete => To delete an exsiting note" << std::endl;
 
 
 };*/
 
-//Command line arguments for cli
-int main(int argc, char *argv[]){
-	
-	int option;
+// Command line arguments for cli
+int main(int argc, char *argv[])
+{
+
+    int option;
     filebackend obj;
     // put ':' in the starting of the
-    // string so that program can 
-    //distinguish between '?' and ':' 
-    while((option = getopt(argc, argv,":ced")) != -1) 
-    { 
-        switch(option) 
-        { 
-            case 'c': 
-            	std::cout << "Creating a new note" << std::endl;
-            	
-            	obj.create();
-            	break; 
-            case 'e':
+    // string so that program can
+    // distinguish between '?' and ':'
+    while ((option = getopt(argc, argv, ":ced")) != -1)
+    {
+        switch (option)
+        {
+        case 'c':
+            std::cout << "Creating a new note" << std::endl;
 
-            	std::cout << "Editing an existing note";
-            	break; 
-            case 'd': 
-                std::cout << "Deleting the note" << std::endl << option; 
-                break; 
-            case 'f': 
-                std::cout << "filename: %s " << std::endl << optarg; 
-                break; 
-            case ':': 
-                std::cout << "option needs a value" << std::endl; 
-                break; 
-            case '?': 
-                //printf(“unknown option: %c\n”, optopt);
-                break; 
-            default: 
-            	exit(0);
-            	break;
-        } 
-    } 
-      
+            obj.create();
+            break;
+        case 'e':
+
+            std::cout << "Editing an existing note";
+            break;
+        case 'd':
+            std::cout << "Deleting the note" << std::endl
+                      << option;
+            break;
+        case 'f':
+            std::cout << "filename: %s " << std::endl
+                      << optarg;
+            break;
+        case ':':
+            std::cout << "option needs a value" << std::endl;
+            break;
+        case '?':
+            // printf(“unknown option: %c\n”, optopt);
+            break;
+        default:
+            exit(0);
+            break;
+        }
+    }
+
     // optind is for the extra arguments
     // which are not parsed
-    for(; optind < argc; optind++){     
-        std::cout << "extra arguments: "<< std::endl << argv[optind]; 
+    for (; optind < argc; optind++)
+    {
+        std::cout << "extra arguments: " << std::endl
+                  << argv[optind];
     }
-      
-	delete obj;
-	
-	return 0;
+
+    
+
+    return 0;
 };
--- filebackend.hpp
+++ filebackend.hpp
@@ -1,45 +1,44 @@
 #pragma once
-//To create and edit in the sqlite db
+// To create and edit in the sqlite db
 #include <string>
 #include <sqlite3.h>
 
+class filebackend
+{
+public:
+    filebackend()
+    {
+        sqlite3 *DB;
+        std::string sql = "CREATE TABLE PASSWORDS("
+                          "ID INT PRIMARY KEY     NOT NULL, "
+                          "USERNAME           TEXT    NOT NULL, "
+                          "PASSWORD          TEXT     NOT NULL;";
+        int exit = 0;
+        exit = sqlite3_open("pwd.db", &DB);
+        char *messaggeError;
+        exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
 
-class filebackend{
-public: 
-	filebackend(){
-	sqlite3* DB;
-    std::string sql = "CREATE TABLE PASSWORDS("
-                      "ID INT PRIMARY KEY     NOT NULL, "
-                      "USERNAME           TEXT    NOT NULL, "
-                      "PASSWORD          TEXT     NOT NULL;";
-    int exit = 0;
-    exit = sqlite3_open("pwd.db", &DB);
-    char* messaggeError;
-    exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messaggeError);
-  
-    if (exit != SQLITE_OK) {
-        std::cerr << "Error Create Table" << std::endl;
-        sqlite3_free(messaggeError);
-    }
-    else
-        std::cout << "Table created Successfully" << std::endl;
-    sqlite3_close(DB);
-    return (0);                  
+        if (exit != SQLITE_OK)
+        {
+            std::cerr << "Error Create Table" << std::endl;
+            sqlite3_free(messaggeError);
+        }
+        else
+            std::cout << "Table created Successfully" << std::endl;
+        sqlite3_close(DB);
+        std::cout << "Closed";
+    };
 
-                  }
-
-}
-void create(){
-	printf("created the file");
-
-	}
-void append(){
-	printf("Appending to the file..");
-
-}	
-void del(){
-	printf("Deleting the file ")
-
-}
-
+    void create()
+    {
+        printf("created the file");
+    };
+    void append()
+    {
+        printf("Appending to the file..");
+    };
+    void del()
+    {
+        printf("Deleting the file ");
+    };
 };
